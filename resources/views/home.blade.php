c4824a27f8162a61c224d4e43717cc1d3e78b0de
Winston Suela <wsuela@gmail.com>
2017-09-09 11:14:17 +0800

    clean up default blade template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,8 @@
 <div class="container">
     <div class="col-md-8 col-md-offset-2">
         <div class="panel panel-default">
-            <div class="panel-heading">Welcome</div>
-
             <div class="panel-body">
-                Hi!
+                Loading...
             </div>
         </div>
     </div>
